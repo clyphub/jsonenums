341733aebef2b41c4592fba0d60a537c56864a07
BenTrapani <btrapani@clypd.com>
2016-08-01 10:08:08 -0400

    Use robust tests provided in gist source

--- jsonenums_test.go
+++ jsonenums_test.go
@@ -4,9 +4,30 @@ import (
 	"testing"
 )
 
-func Test_ToSnake(t *testing.T) {
-	sample1 := "aBcDEfg"
-	if "a_bc_d_efg" != ToSnake(sample1) {
-		t.Fail()
+type SnakeTest struct {
+	input  string
+	output string
+}
+
+var tests = []SnakeTest{
+	{"a", "a"},
+	{"snake", "snake"},
+	{"A", "a"},
+	{"ID", "id"},
+	{"MOTD", "motd"},
+	{"Snake", "snake"},
+	{"SnakeTest", "snake_test"},
+	{"SnakeID", "snake_id"},
+	{"SnakeIDGoogle", "snake_id_google"},
+	{"LinuxMOTD", "linux_motd"},
+	{"OMGWTFBBQ", "omgwtfbbq"},
+	{"omg_wtf_bbq", "omg_wtf_bbq"},
+}
+
+func TestToSnake(t *testing.T) {
+	for _, test := range tests {
+		if ToSnake(test.input) != test.output {
+			t.Errorf(`ToSnake("%s"), wanted "%s", got \%s"`, test.input, test.output, ToSnake(test.input))
+		}
 	}
 }
